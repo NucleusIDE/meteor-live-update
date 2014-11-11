527de30e11b3faae26fdd05942a3922ff2956508
Charanjit Singh <ckhabra007@gmail.com>
2014-11-11 15:28:22 +0530

    Updated package.js for new meteor package definitions

--- package.js
+++ package.js
@@ -1,13 +1,18 @@
 Package.describe({
-    summary: "Live update the changes in browser without full page reload"
+  summary: "Live update the changes in browser without full page reload",
+  version: "0.1.0",
+  name: "channikhabra:live-update",
+  git: 'https://github.com/channikhabra/meteor-live-update'
 });
 
 Package.on_use(function (api, where) {
-    api.use(['reload', 'ui', 'spacebars-compiler', 'autoupdate', 'deps']);
+  api.versionsFrom("METEOR@0.9.1");
 
-    api.add_files([
-        'js/live-update.js'
-    ], ['client']);
+  api.use(['reload', 'ui', 'spacebars-compiler', 'autoupdate', 'deps']);
 
-    api.export && api.export(['LiveUpdate', 'LiveUpdateParser', 'LiveUpdateCache'],['client']);
+  api.add_files([
+    'js/live-update.js'
+  ], ['client']);
+
+  api.export && api.export(['LiveUpdate', 'LiveUpdateParser', 'LiveUpdateCache'],['client']);
 });
